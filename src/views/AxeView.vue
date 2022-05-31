681e739395872d3b70d251e039410a0dfb54292c
LionelNanchenWE <lionel.nanchen@webevolutions.ch>
2022-05-31 17:02:28 +0200

    Axe: add step enum

--- src/views/AxeView.vue
+++ src/views/AxeView.vue
@@ -11,6 +11,13 @@ interface Data {
     isCutting: boolean,
 }
 
+enum Step {
+    First = 10,
+    Second = 20,
+    Third = 30,
+    Final = 50,
+}
+
 export default defineComponent({
     components: { Container },
     data() {
@@ -22,11 +29,10 @@ export default defineComponent({
     },
     methods: {
         time() {
-            if (this.logs < 10) return 200;
-            else if (this.logs < 15) return 400;
-            else if (this.logs < 20) return 750;
-            else if (this.logs < 30) return 1000;
-            else return 2000;
+            if (this.logs < Step.First) return 300;
+            else if (this.logs < Step.Second) return 500;
+            else if (this.logs < Step.Third) return 800;
+            else return 1200;
         },
         onCut() {
             this.isCutting = true;
@@ -38,10 +44,10 @@ export default defineComponent({
     },
     computed: {
         treeTrunk() {
-            return this.logs < 10 ? TreeTrunk : TreeTrunkCut
+            return this.logs < Step.First ? TreeTrunk : TreeTrunkCut
         },
         axe() {
-            return this.logs < 20 ? Axe1 : Axe2;
+            return this.logs < Step.Second ? Axe1 : Axe2;
         },
     },
 });
@@ -60,13 +66,13 @@ export default defineComponent({
                 </el-button>
             </div>
             <div style="margin-top: 12px">
-                <el-alert v-if="logs >= 10 && logs < 20" title="Crac ... boom" type="warning" :closable="false"
+                <el-alert v-if="logs >= Step.First && logs < Step.Second" title="Crac ... boom" type="warning" :closable="false"
                     effect="dark" show-icon />
-                <el-alert v-if="logs >= 20 && logs < 30" title="Changement d'hache" type="warning" :closable="false"
+                <el-alert v-if="logs >= Step.Second && logs < Step.Third" title="Changement d'hache" type="warning" :closable="false"
                     effect="dark" show-icon />
-                <el-alert v-if="logs >= 30 && logs < 100" title="Pas très utile ..." type="error" :closable="false" effect="dark"
+                <el-alert v-if="logs >= Step.Third && logs < Step.Final" title="Pas très utile ..." type="error" :closable="false" effect="dark"
                     show-icon />
-                <el-alert v-if="logs >= 100" title="Persévérant ... mais inutile" type="error" :closable="false" effect="dark"
+                <el-alert v-if="logs >= Step.Final" title="Mais laisse cet arbre tranquille! La réponse est devant tes yeux!" type="error" :closable="false" effect="dark"
                     show-icon />
             </div>
         </el-card>
